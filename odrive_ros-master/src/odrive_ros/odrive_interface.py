fae4842e6e4b4395afe32ce637b892c833b23ce4
mertsolkiran <mert.solkiran@berge.io>
2019-06-05 12:11:26 +0100

    Odrive Serial Number Fix

--- odrive_ros-master/src/odrive_ros/odrive_interface.py
+++ odrive_ros-master/src/odrive_ros/odrive_interface.py
@@ -40,11 +40,15 @@ class ODriveInterfaceAPI(object):
     def __del__(self):
         self.disconnect()
                     
-    def connect(self, port=None, right_axis=0, timeout=30):
-        if self.driver:
+    def connect(self, port=None, right_axis=0, timeout=30, serial_number=None):
+	if serial_number is None:
+            self.logger.error("No serial number was given!")
+            return False
+        
+	if self.driver:
             self.logger.info("Already connected. Disconnecting and reconnecting.")
         try:
-            self.driver = odrive.find_any(timeout=timeout, logger=self.logger)
+            self.driver = odrive.find_any(serial_number=serial_number, timeout=timeout, logger=self.logger)
             self.axes = (self.driver.axis0, self.driver.axis1)
         except:
             self.logger.error("No ODrive found. Is device powered?")
